14453e6a44c2c6821fac87108b0506ca3201f666
ALevsha <levsha26r@gmail.com>
2023-04-11 21:40:31 +0300

    add http interceptor for getting information about http request

--- app/src/main/java/com/example/retrofitapp/MainActivity.kt
+++ app/src/main/java/com/example/retrofitapp/MainActivity.kt
@@ -10,6 +10,8 @@ import com.example.retrofitapp.retrofit.ProductApi
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
@@ -24,8 +26,18 @@ class MainActivity : AppCompatActivity() {
         val b1 = findViewById<Button>(R.id.b1)
         val et = findViewById<EditText>(R.id.etNumber)
 
+        //creating interceptor with logging level
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        //creating Httpclient with interceptor
+        val client = OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .build()
+
         val retrofit = Retrofit.Builder()
             .baseUrl("https://dummyjson.com")
+            .client(client) // add client in retrofit object
             .addConverterFactory(GsonConverterFactory.create())
             .build()
         val productApi = retrofit.create(ProductApi::class.java)
@@ -36,7 +48,7 @@ class MainActivity : AppCompatActivity() {
                 val product = productApi.getProductById(id = if (et.text.isEmpty()) 1
                                                             else et.text.toString().toInt())
                 runOnUiThread{
-                    tv1.text = product.brand
+                    tv1.text = product.title
                 }
             }
         }
